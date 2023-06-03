5ab511a52ebd0a8d7c3498afc93114c7fca4c82b
Hyemin Lee <cotloz88@gmail.com>
2023-06-04 05:47:54 +0900

    implement project3

---
+++ project3/loader.py
@@ -0,0 +1,232 @@
+'''
+bvh 파일 parser
+'''
+from OpenGL.GL import *
+from glfw.GLFW import *
+import glm
+import ctypes
+import numpy as np
+from node import Node as Joint
+import os
+
+class Loader:
+    def __init__(self):
+        self.__filepath = ""
+        self.__root = None
+        
+        self.__is_fill = False
+        self.__is_animating = False
+
+        self.frames = 0
+        self.frame_time = 1
+        
+        # for debugging...
+        self.__total_frame_cnt = 0
+        self.__channel_cnt = 0
+        self.__total_joint_cnt = 0
+
+    @property
+    def root(self):
+        return self.__root
+
+    @property
+    def is_fill(self):
+        return self.__is_fill
+    
+    @property
+    def is_animating(self):
+        return self.__is_animating
+
+    def is_float(self, num):
+        try:
+            float(num)
+            return True
+        except ValueError:
+            return False
+
+    def change_is_fill(self, new_is_fill):
+        self.__is_fill = new_is_fill
+
+    def change_is_animating(self):
+        self.__is_animating = not self.__is_animating
+
+    def parse_channel_data(self, channel_data):
+        '''
+        DFS로 root부터 돌면서 channel data를 파싱하여 넣어줌
+        한 프레임에 대한 channel data를 파싱
+        '''
+        visited = []
+        channel_stack = [self.__root]
+
+        data_cnt = 0
+
+        while channel_stack:
+            current_node = channel_stack.pop()
+            if current_node not in visited:
+                visited.append(current_node)
+                channel_data_per_frame = channel_data[data_cnt:data_cnt + len(current_node.channels)]
+                data_cnt += len(current_node.channels)
+                current_node.append_joint_transform(channel_data_per_frame)
+
+                for child in reversed(current_node.children):
+                    channel_stack.append(child)
+
+    def parse_bvh(self, filepath):
+        # initialize member variables
+        self.__is_animating = False
+        self.__filepath = ""
+        self.__root = None
+
+        self.frames = 0
+        self.frame_time = 1
+        
+        # for debugging...
+        self.__total_frame_cnt = 0
+        self.__channel_cnt = 0
+        self.__total_joint_cnt = 0
+
+        with open(filepath, 'r') as f:
+            lines = f.readlines()
+            
+            parent_joint = None
+            current_joint = None
+
+            for line in lines:
+                words = line.split()
+
+                if len(words) < 1:
+                    continue
+                
+                # Hierarchy에서 가능한 옵션
+                # - ROOT: Root node를 가리킴
+                # - {: 새로운 하위 노드의 시작을 가리킴
+                # - OFFSET: static data인 translational data를 가리킴
+                # - CHANNELS: dynamic data인 Rotational data를 가리키기 위함
+                # - JOINT: 해당 Joint의 이름을 나타냄
+                # - }: 중괄호가 닫힐 때, 해당 노드는 괄호 상위 노드의 자식임을 가리키게 됨
+                #      직전의 노드가 부모가 아니라. 상위 노드가 부모
+                # - End Site: End effector
+                
+                if words[0] == '{':
+                    parent_joint = current_joint
+
+                elif words[0] == '}':
+                    current_joint = current_joint.parent
+                    parent_joint = current_joint
+
+                elif words[0] == 'OFFSET':
+                    current_joint.set_link_transformation(glm.translate(glm.vec3(float(words[1]), float(words[2]), float(words[3]))))
+
+                elif words[0] == 'CHANNELS':
+                    total_length = int(words[1])
+                    self.__channel_cnt += total_length
+                    for i in range(total_length):
+                        current_joint.channels.append(words[i + 2])
+
+                elif words[0] == 'ROOT' or words[0] == 'JOINT' or words[0] == 'End':
+                    joint_name = words[1]
+
+                    if words[0] == 'End':
+                        joint_name = words[0] + " " + words[1]
+                        self.__total_joint_cnt -= 1
+
+                    current_joint = Joint(parent_joint, joint_name, glm.vec3(1,1,1)) # TODO: End site는 이름 설정 X
+                    self.__total_joint_cnt += 1
+                    
+                    if words[0] == 'ROOT':
+                        self.__root = current_joint
+
+                # Motion에서 가능한 옵션
+                # - Frames: 총 pose의 개수
+                # - Frame Time: FPS
+                # - 이외의 숫자 옵션: 각 column의 offset을 저장함
+ 
+                elif words[0] == 'Frames:':
+                    self.frames = int(words[1])
+                
+                elif words[0] == 'Frame' and words[1] == 'Time:':
+                    self.frame_time = float(words[2])
+                
+                elif self.is_float(words[0]):
+                    self.__total_frame_cnt += 1
+                    self.parse_channel_data(words)
+
+        self.__filepath = filepath
+
+    def print_bvh_data(self):
+        print("---------------------------------------------")
+        print("file name: " + os.path.basename(str(self.__filepath))) # TODO: p2 참고해서 filename 파싱
+        print("number of frames: " + str(self.__total_frame_cnt))
+        print("FPS: " + str(1 / self.frame_time))
+        print("number of joints: " + str(self.__total_joint_cnt))
+
+        visited = []
+        dfs_joint_name = []
+        joint_stack = [self.__root]
+
+        while joint_stack:
+            current_node = joint_stack.pop()
+            if current_node not in visited:
+                visited.append(current_node)
+
+                if current_node.joint_name != "End Site": # TODO: Joint에 End effector도 포함되나?
+                    dfs_joint_name.append(current_node.joint_name)
+
+                for child in reversed(current_node.children):
+                    joint_stack.append(child)
+
+        print("all of joint name: " + str(dfs_joint_name))
+
+    def prepare_vaos_line(self):
+        visited = []
+        joint_stack = [self.__root]
+
+        while joint_stack:
+            current_node = joint_stack.pop()
+            if current_node not in visited:
+                visited.append(current_node)
+                current_node.prepare_vao_line()
+
+                for child in reversed(current_node.children):
+                    joint_stack.append(child)
+        
+        self.__root.update_tree_global_transform_skeleton()
+
+    def prepare_vaos_box(self):
+        visited = []
+        channel_stack = [self.__root]
+
+        while channel_stack:
+            current_node = channel_stack.pop()
+            if current_node not in visited:
+                visited.append(current_node)
+                current_node.prepare_vao_box()
+
+                for child in reversed(current_node.children):
+                    channel_stack.append(child)
+        
+        self.__root.update_tree_global_transform_skeleton()
+
+    def draw_animation(self, VP, MVP_loc, color_loc, frame, M_loc):
+        '''
+        그려야하는 box 개수만큼(root + joint 개수만큼),
+        joint 배열을 순회하면서 해당 joint node의 draw를 호출
+        '''
+        if self.__is_animating:
+            self.__root.update_tree_global_transform(frame)
+
+        visited = []
+        channel_stack = [self.__root]
+
+        while channel_stack:
+            current_node = channel_stack.pop()
+            if current_node not in visited:
+                visited.append(current_node)
+
+                if self.__is_fill:
+                    current_node.draw_node_box(VP, MVP_loc, color_loc, M_loc)
+                else:
+                    current_node.draw_node_line(VP, MVP_loc, color_loc)
+
+                for child in reversed(current_node.children):
+                    channel_stack.append(child)
---
+++ project3/node.py
@@ -0,0 +1,231 @@
+from OpenGL.GL import *
+from glfw.GLFW import *
+import glm
+import ctypes
+import numpy as np
+
+class Node:
+    def __init__(self, parent, node_name, color):
+        # hierarchy
+        self.parent = parent
+        self.children = []
+        if parent is not None:
+            parent.children.append(self)
+
+        # link transform (static data)
+        self.link_transform_from_parent = glm.mat4() # translation matrix
+        
+        # joint transform (dynamic data)
+        self.channels = [] 
+        self.joint_transform = []
+
+        # global transformation matrix for calculating
+        self.global_transform = glm.mat4()
+        # color
+        self.color = color
+
+        # name
+        self.joint_name = node_name
+
+        # calculated vao for each node
+        self.vao_line = None
+        self.vao_box = None
+
+    def set_link_transformation(self, link_transformation):
+        self.link_transform_from_parent = link_transformation
+
+    def append_joint_transform(self, joint_transforms):
+        # TODO: 곱하는 순서가 position 먼저인지 rotation 먼저인지
+        T_x = 0
+        T_y = 0
+        T_z = 0
+        R = glm.mat4()
+
+        for i, joint_transform in enumerate(joint_transforms):
+            channel_i = self.channels[i].lower()
+            if channel_i == 'xposition':
+                T_x = float(joint_transform)
+            elif channel_i == 'yposition':
+                T_y = float(joint_transform)
+            elif channel_i == 'zposition':
+                T_z = float(joint_transform)
+            elif channel_i == 'xrotation':
+                R = R * glm.rotate(glm.radians(float(joint_transform)), (1, 0, 0))
+            elif channel_i == 'yrotation':
+                R = R * glm.rotate(glm.radians(float(joint_transform)), (0, 1, 0))
+            elif channel_i == 'zrotation':
+                R = R * glm.rotate(glm.radians(float(joint_transform)), (0, 0, 1))
+
+        self.joint_transform.append(glm.translate(glm.vec3(T_x, T_y, T_z)) * R)
+
+    def get_global_transform(self):
+        return self.global_transform
+    
+    def get_color(self):
+        return self.color
+    
+    def update_tree_global_transform_skeleton(self):
+        if self.parent is not None:
+            self.global_transform = self.parent.get_global_transform() * self.link_transform_from_parent
+        else:
+            self.global_transform = self.link_transform_from_parent
+
+        for child in self.children:
+            child.update_tree_global_transform_skeleton()
+    
+    def update_tree_global_transform(self, frame):
+        if self.parent is not None:
+            self.global_transform = self.parent.get_global_transform() * self.link_transform_from_parent * self.joint_transform[frame]
+        else:
+            self.global_transform = self.link_transform_from_parent * self.joint_transform[frame]
+
+        for child in self.children:
+            child.update_tree_global_transform(frame)
+
+    def prepare_vao_line(self):
+        # prepare vertex data (in main memory)
+        vertices = glm.array(glm.float32,
+            # position        # color
+            0.0, 0.0, 0.0,  1.0, 0.5, 1.0, # line start
+            self.link_transform_from_parent[3].x, self.link_transform_from_parent[3].y, self.link_transform_from_parent[3].z,  1.0, 0.5, 1.0, # line end(each of them is offset, xyz value)
+        )
+
+        # create and activate VAO (vertex array object)
+        VAO = glGenVertexArrays(1)  # create a vertex array object ID and store it to VAO variable
+        glBindVertexArray(VAO)      # activate VAO
+
+        # create and activate VBO (vertex buffer object)
+        VBO = glGenBuffers(1)   # create a buffer object ID and store it to VBO variable
+        glBindBuffer(GL_ARRAY_BUFFER, VBO)  # activate VBO as a vertex buffer object
+
+        # copy vertex data to VBO
+        glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices.ptr, GL_STATIC_DRAW) # allocate GPU memory for and copy vertex data to the currently bound vertex buffer
+
+        # configure vertex positions
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * glm.sizeof(glm.float32), None)
+        glEnableVertexAttribArray(0)
+
+        # configure vertex colors
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * glm.sizeof(glm.float32), ctypes.c_void_p(3*glm.sizeof(glm.float32)))
+        glEnableVertexAttribArray(1)
+
+        self.vao_line = VAO
+    
+    def prepare_vao_box(self):
+        # prepare vertex data (in main memory)
+        # 36 vertices for 12 triangles
+        thickness = 0.5
+
+        offset_x = self.link_transform_from_parent[3].x
+        offset_y = self.link_transform_from_parent[3].y
+        offset_z = self.link_transform_from_parent[3].z
+
+        # offset이 y방향만 있는게 아니라 x, y, z 다 있으므로 그걸 고려해줘야함 
+        height = glm.sqrt(glm.pow(offset_x, 2) + glm.pow(offset_y, 2) + glm.pow(offset_z, 2))
+
+        cuboid_vertices = [
+            [-thickness, height, thickness], # v0
+            [thickness, height, thickness], # v1
+            [thickness, 0, thickness], # v2
+            [-thickness, 0,  thickness], # v3
+            [-thickness , height, -thickness], # v4
+            [thickness, height, -thickness], # v5
+            [thickness, 0, -thickness], # v6
+            [-thickness, 0, -thickness], # v7
+        ]
+
+        for idx, cuboid_vertex in enumerate(cuboid_vertices):
+            if offset_x == 0 and offset_z == 0:
+                if offset_y < 0:
+                    cuboid_vertices[idx][1] *= -1
+            else:
+                offset_vector = glm.normalize(glm.vec3(offset_x, offset_y, offset_z))
+                normal_vector = glm.normalize(glm.cross(glm.vec3(0, 1, 0), offset_vector))
+                cos_theta = glm.dot(glm.vec3(0, 1, 0), offset_vector)
+
+                R = glm.rotate(glm.acos(cos_theta), normal_vector)
+                cuboid_vertices[idx] = R * cuboid_vertex
+
+        cuboid_indices = [
+            [0,2,1],
+            [0,3,2],
+            [4,5,6],
+            [4,6,7],
+            [0,1,5],
+            [0,5,4],
+            [3,6,2],
+            [3,7,6],
+            [1,2,6],
+            [1,6,5],
+            [0,7,3],
+            [0,4,7],
+        ]
+        
+        color = [1.0, 0.5, 1.0]
+
+        vertices = []
+
+        for idx, cuboid_index in enumerate(cuboid_indices):
+            vector1 = glm.vec3(cuboid_vertices[cuboid_index[1]]) - glm.vec3(cuboid_vertices[cuboid_index[0]])
+            vector2 = glm.vec3(cuboid_vertices[cuboid_index[2]]) - glm.vec3(cuboid_vertices[cuboid_index[0]])
+            one_vnormal = glm.normalize(glm.cross(vector1, vector2))
+            for index in cuboid_index:
+                vertices.append([cuboid_vertices[index][0], cuboid_vertices[index][1], cuboid_vertices[index][2], color[0], color[1], color[2], one_vnormal[0], one_vnormal[1], one_vnormal[2]])
+
+        vertices = np.concatenate(np.array(vertices, dtype='f4'))
+        vertices = glm.array(vertices)
+
+        # create and activate VAO (vertex array object)
+        VAO = glGenVertexArrays(1)  # create a vertex array object ID and store it to VAO variable
+        glBindVertexArray(VAO)      # activate VAO
+
+        # create and activate VBO (vertex buffer object)
+        VBO = glGenBuffers(1)   # create a buffer object ID and store it to VBO variable
+        glBindBuffer(GL_ARRAY_BUFFER, VBO)  # activate VBO as a vertex buffer object
+
+        # copy vertex data to VBO
+        glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices.ptr, GL_STATIC_DRAW) # allocate GPU memory for and copy vertex data to the currently bound vertex buffer
+
+        # configure vertex positions
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * glm.sizeof(glm.float32), None)
+        glEnableVertexAttribArray(0)
+
+        # configure vertex color
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 9 * glm.sizeof(glm.float32), ctypes.c_void_p(3*glm.sizeof(glm.float32)))
+        glEnableVertexAttribArray(1)
+
+        # configure vertex normal
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 9 * glm.sizeof(glm.float32), ctypes.c_void_p(6*glm.sizeof(glm.float32)))
+        glEnableVertexAttribArray(2)
+
+        self.vao_box = VAO
+
+    def draw_node_line(self, VP, MVP_loc, color_loc):
+        MVP = glm.mat4()
+
+        if self.parent is not None:
+            MVP = VP * self.parent.get_global_transform()
+        else:
+            MVP = VP * self.get_global_transform()
+        color = self.get_color()
+
+        glBindVertexArray(self.vao_line)
+        glUniformMatrix4fv(MVP_loc, 1, GL_FALSE, glm.value_ptr(MVP))
+        glUniform3f(color_loc, color.r, color.g, color.b)
+        glDrawArrays(GL_LINES, 0, 2)
+
+    def draw_node_box(self, VP, MVP_loc, color_loc, M_loc):
+        M = glm.mat4()
+        if self.parent is not None:
+            M = self.parent.get_global_transform()
+        else:
+            M = self.get_global_transform()
+
+        MVP = VP * M
+        color = self.get_color()
+
+        glBindVertexArray(self.vao_box)
+        glUniformMatrix4fv(MVP_loc, 1, GL_FALSE, glm.value_ptr(MVP))        
+        glUniformMatrix4fv(M_loc, 1, GL_FALSE, glm.value_ptr(M))
+        glUniform3f(color_loc, color.r, color.g, color.b)
+        glDrawArrays(GL_TRIANGLES, 0, 36)
